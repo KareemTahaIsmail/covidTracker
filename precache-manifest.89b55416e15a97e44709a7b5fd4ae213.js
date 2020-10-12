6799dd0ff80e587ce2685b34715fef7059e415fc
Kareem Ismail <KareemTahaIsmail@gmail.com>
2020-10-12 18:17:27 -0400

    Updates

---
+++ precache-manifest.89b55416e15a97e44709a7b5fd4ae213.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31ee0ae7fed4ce10addabba0cccc9e51",
+    "url": "/covidTracker/index.html"
+  },
+  {
+    "revision": "ef95f3e6e3aecf00d785",
+    "url": "/covidTracker/static/css/2.067ccc96.chunk.css"
+  },
+  {
+    "revision": "dc82b681ba8a46e1e517",
+    "url": "/covidTracker/static/css/main.880de404.chunk.css"
+  },
+  {
+    "revision": "ef95f3e6e3aecf00d785",
+    "url": "/covidTracker/static/js/2.4b0153b4.chunk.js"
+  },
+  {
+    "revision": "af5492f10234ec8ba29a624c4fe6fb11",
+    "url": "/covidTracker/static/js/2.4b0153b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc82b681ba8a46e1e517",
+    "url": "/covidTracker/static/js/main.4c66b62b.chunk.js"
+  },
+  {
+    "revision": "f38490e7251ca84f7e01",
+    "url": "/covidTracker/static/js/runtime-main.553ab019.js"
+  }
+]);
