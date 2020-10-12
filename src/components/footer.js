11cc98ee8bdb34ddabdde85d83abc805017c0a69
Kareem Ismail <54039939+KareemTahaIsmail@users.noreply.github.com>
2020-10-12 18:01:25 -0400

    Add files via upload

---
+++ src/components/footer.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './footer.css';
+
+
+const Footer = () => {
+    return(
+        <footer>
+            <ul>
+                <li><a href=""/>LinkedIn</li>
+                <li><a href=""/>LinkedIn</li>
+                <li><a href=""/>LinkedIn</li>
+            </ul>
+        </footer>
+    )
+}
+
+export default Footer;
